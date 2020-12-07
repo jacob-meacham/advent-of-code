3255c8357f7e92e3a0f4d360a689e226fd2a5a6b
Jacob Meacham <jacob.e.meacham@gmail.com>
2020-12-06 21:52:44 -0700

    Add stubs for all solutions

---
+++ 2020/day-7/src/main.rs
@@ -0,0 +1,15 @@
+use common::benchmarking::benchmark_test;
+
+fn test<'a, I>(_lines: I) -> (i32, i32) where I: Iterator<Item = &'a String> {
+    (0, 0)
+}
+
+fn main() {
+    let lines = common::input::open_input_as_vector();
+    let (p1, p2) = test(lines.iter());
+
+    println!("❌ Part 1: {}", p1);
+    println!("❌ Part 2: {}", p2);
+
+    benchmark_test(|| { test(lines.iter().clone()); });
+}
