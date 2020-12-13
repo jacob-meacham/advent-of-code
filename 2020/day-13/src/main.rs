768e71c533c7f7ad471ff6216038e7d5bd039b54
Jacob Meacham <jacob.e.meacham@gmail.com>
2020-12-13 11:47:06 -0700

    Day 13

--- 2020/day-13/src/main.rs
+++ 2020/day-13/src/main.rs
@@ -1,15 +1,37 @@
 use common::benchmarking::benchmark_test;
+use std::str::FromStr;
 
-// Just treat the slices as binary numbers
-fn test<'a, I>(_lines: I) -> (i32, i32) where I: Iterator<Item = &'a String> {
-    (0, 0)
+fn test(lines: &Vec<String>) -> (u32, u64) {
+    let timestamp = u32::from_str(&lines[0]).unwrap();
+    let parsed_lines = &lines[1].split(',').map(|s| {
+        match u32::from_str(s) {
+            Ok(x) => Some(x),
+            Err(_) => None
+        }
+    }).collect::<Vec<_>>();
+
+    let (min_n, min_timestamp) = parsed_lines.iter().filter_map(|x| *x)
+        .map(|n| {
+        (n, (timestamp / n + 1) * n)
+    }).min_by(|(_, xt), (_, yt)| xt.cmp(yt)).unwrap();
+
+    // Sieve-esque
+    let buses: Vec<(u64, u64)> = parsed_lines.iter().enumerate()
+        .filter_map(|(index, n)| n.map_or(None,|b| Some((b as u64, index as u64)))).collect();
+    let p2 = buses[1..].iter().fold((buses[0].0, buses[0].0), |(cur_num, multiple), bus| {
+        let new_num = (cur_num..).step_by(multiple as usize)
+            .filter(|n| (n + bus.1) % bus.0 == 0).next().unwrap();
+        (new_num, multiple * bus.0)
+    });
+
+    ((min_timestamp - timestamp) * min_n, p2.0)
 }
 fn main() {
     let lines = common::input::open_input_as_vector();
-    let (p1, p2) = test(lines.iter());
+    let (p1, p2) = test(&lines);
 
-    println!("❌ Part 1: {}", p1);
-    println!("❌ Part 2: {}", p2);
+    println!("✅ Part 1: {}", p1);
+    println!("✅ Part 2: {}", p2);
 
-    benchmark_test(|| { test(lines.iter().clone()); });
+    benchmark_test(|| { test(&lines); });
 }
