e3325b8b659d3fa34f8f777935bbb0fd0dc63fe6
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-01-17 03:30:24 -0700

    Day 1 from 38000 feet

---
+++ 2022/day-1/main.py
@@ -0,0 +1,34 @@
+from functools import reduce
+
+
+def part1(elf_inventories):
+    def fn(acc, xs):
+        s = sum(xs)
+        return max(s, acc)
+    return reduce(fn, elf_inventories, 0)
+
+
+# Obviously could solve p1 with this too
+def part2(elf_inventories):
+    sums = sorted([sum(e) for e in elf_inventories], reverse=True)
+    return sum(sums[0:3])
+
+
+def main():
+    elf_inventories = [[]]
+    # TODO: Need to fix the folder issue
+    with open('day-1/input.txt', 'r') as f:
+        for l in f.readlines():
+            l = l.strip()
+            if not l:
+                elf_inventories.append([])
+            else:
+                elf_inventories[-1].append(int(l))
+
+    p1 = part1(elf_inventories)
+    p2 = part2(elf_inventories)
+    return p1, p2
+
+
+if __name__ == '__main__':
+    main()
