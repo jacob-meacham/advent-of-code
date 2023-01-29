8ec8dea7bdd6e8a4be6c93ed1c962a5dfbb0ffe8
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-01-28 21:23:49 -0700

    Add day 4

---
+++ 2022/day-4/main.py
@@ -0,0 +1,43 @@
+import re
+
+
+def is_assignment_contained(min0, max0, min1, max1):
+    if min0 >= min1 and max0 <= max1:
+        return 1
+
+    if min1 >= min0 and max1 <= max0:
+        return 1
+
+    return 0
+
+
+def is_overlap(min0, max0, min1, max1):
+    if min0 > max1 or min1 > max0:
+        return 0
+
+    return 1
+
+
+def part1(assignments):
+    contained = [is_assignment_contained(min0, max0, min1, max1) for ((min0, max0), (min1, max1)) in assignments]
+    return sum(contained)
+
+
+def part2(assignments):
+    overlaps = [is_overlap(min0, max0, min1, max1) for ((min0, max0), (min1, max1)) in assignments]
+    return sum(overlaps)
+
+
+def main():
+    # TODO: Need to fix the folder issue
+    with open('day-4/input.txt', 'r') as f:
+        matches = [re.match(r'(\d+)-(\d+),(\d+)-(\d+)', l.strip()) for l in f.readlines()]
+        assignments = [((int(m.group(1)), int(m.group(2))), (int(m.group(3)), int(m.group(4)))) for m in matches]
+
+    p1 = part1(assignments)
+    p2 = part2(assignments)
+    return p1, p2
+
+
+if __name__ == '__main__':
+    main()
