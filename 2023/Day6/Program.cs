4c2a63d362c7eebf7f5fe28873a81a35fc4d1319
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-10 15:41:03 -0700

    Day 6 2023

--- 2023/Day6/Program.cs
+++ 2023/Day6/Program.cs
@@ -1,13 +1,50 @@
-﻿using Utilities;
+﻿using System.Text.RegularExpressions;
+using Utilities;
 
-int Part1(List<String> lines)
+long SolveRace(long time, long distance)
 {
-    return 0;
+    // Quadratic equation of the form (time-x)(x) = distance
+    // -x2+time-distance = 0
+    var a = -1L;
+    var b = time;
+    var c = -distance;
+        
+    // Assuming well-formed input
+    double discriminant = b * b - 4 * a * c;
+    double sqrtDiscriminant = Math.Sqrt(discriminant);
+    var x1 = (long)Math.Ceiling((-b + sqrtDiscriminant) / (2 * a));
+    var x2 = (long)Math.Floor((-b - sqrtDiscriminant) / (2 * a));
+
+    // Need to best the time
+    if ((time - x1) * x1 <= distance)
+    {
+        x1 += 1;
+    }
+        
+    if ((time - x2) * x2 <= distance)
+    {
+        x2 -= 1;
+    }
+
+    return x2 - x1 + 1L;
 }
 
-int Part2(List<String> lines)
+long Part1(List<string> lines)
 {
-    return 0;
+    var races = LineRegex().Matches(lines[0])
+        .Select(m => long.Parse(m.Value))
+        .Zip(LineRegex().Matches(lines[1]).Select(m => long.Parse(m.Value))).ToList();
+    
+    return races
+        .Select(race => SolveRace(race.First, race.Second))
+        .Aggregate(1L, (acc, n) => acc * n);
+}
+
+long Part2(List<string> lines)
+{
+    long time = long.Parse(string.Join("", lines[0].Split(": ")[1].Split(" ")));
+    long distance = long.Parse(string.Join("", lines[1].Split(": ")[1].Split(" ")));
+    return SolveRace(time, distance);
 }
 
 var lines = new List<string>(File.ReadAllLines("input.txt"));
@@ -21,3 +58,9 @@ Runner.Benchmark(delegate
     Part1(lines);
     Part2(lines);
 }, "Day 6");
+
+partial class Program
+{
+    [GeneratedRegex(@"\d+")]
+    private static partial Regex LineRegex();
+}
--- 2023/README.md
+++ 2023/README.md
@@ -9,5 +9,7 @@ Using this as an excuse to get better at modern C# development.
 | Day 1  | 2.6        |✅      |
 | Day 2  | 4.5        |✅      |
 | Day 3  | 4.1        |✅      |
-
+| Day 4  | 2.8        |✅      |
+| Day 5  | 1.1        |✅      |
+| Day 6  | 0          |✅      |
 
