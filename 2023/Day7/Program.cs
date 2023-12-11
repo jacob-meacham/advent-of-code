80f315057c700f4fbe29509531c86c59ba442786
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-10 21:14:14 -0700

    Day 7 2023 (+ some Day 6 cleanup)

--- 2023/Day7/Program.cs
+++ 2023/Day7/Program.cs
@@ -1,13 +1,30 @@
 ﻿using Utilities;
 
-int Part1(List<String> lines)
+long GetTotalValue(List<string> lines, bool jacksWild)
 {
-    return 0;
+    var hands = lines.Select(l =>
+    {
+        var split = l.Split(" ");
+        return new Hand(split[0], long.Parse(split[1]), jacksWild);
+    }).Order();
+    
+    // the tuple is (rank, winnings)
+    var result = hands.Aggregate((1L, 0L), (tuple, hand) =>
+    {
+        return (tuple.Item1 + 1, tuple.Item2 + tuple.Item1 * hand.Bid);
+    });
+    
+    return result.Item2;
 }
 
-int Part2(List<String> lines)
+long Part1(List<string> lines)
 {
-    return 0;
+    return GetTotalValue(lines, false);
+}
+
+long Part2(List<string> lines)
+{
+    return GetTotalValue(lines, true);
 }
 
 var lines = new List<string>(File.ReadAllLines("input.txt"));
@@ -21,3 +38,186 @@ Runner.Benchmark(delegate
     Part1(lines);
     Part2(lines);
 }, "Day 7");
+
+internal enum HandType
+{
+    HighCard = 0,
+    Pair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind
+}
+
+class Hand : IComparable<Hand>
+{
+    private HandType Type { get; }
+    private string Value { get; }
+    public long Bid { get; }
+
+    private bool JacksWild { get; }
+
+    private HandType GetType(string value)
+    {
+        var cardCounts = new Dictionary<string, int>();
+        foreach (var c in value)
+        {
+            if (cardCounts.ContainsKey(c.ToString()))
+            {
+                cardCounts[c.ToString()]++;
+            }
+            else
+            {
+                cardCounts[c.ToString()] = 1;
+            }
+        }
+
+        if (JacksWild)
+        {
+            return GetTypeWild(cardCounts);
+        }
+        
+        return GetTypeNormal(cardCounts);
+    }
+
+    private static HandType GetTypeWild(Dictionary<string, int> cardCounts)
+    {
+        long numJacks = cardCounts.GetValueOrDefault("J");
+        var counts = new List<int>();
+        foreach (var key in cardCounts.Keys)
+        {
+            if (key != "J")
+            {
+                counts.Add(cardCounts[key]);
+            }
+        }
+
+        counts = counts.OrderByDescending(n => n).ToList();
+        var most = counts.ElementAtOrDefault(0) + numJacks;
+
+        if (most == 5)
+        {
+            return HandType.FiveOfAKind;
+        }
+        
+        if (most == 4)
+        {
+            return HandType.FourOfAKind;
+        }
+        
+        if (most == 3 && counts.Skip(1).Contains(2))
+        {
+            return HandType.FullHouse;
+        }
+        
+        if (most == 3)
+        {
+            return HandType.ThreeOfAKind;
+        }
+        
+        if (most == 2 && counts.Skip(1).Contains(2))
+        {
+            return HandType.TwoPair;
+        }
+        
+        if (most == 2)
+        {
+            return HandType.Pair;
+        }
+        
+        return HandType.HighCard;
+    }
+
+    private HandType GetTypeNormal(Dictionary<string, int> cardCounts)
+    {
+        var countsSet = new HashSet<int>(cardCounts.Values);
+        if (countsSet.Contains(5))
+        {
+            return HandType.FiveOfAKind;
+        }
+        
+        if (countsSet.Contains(4))
+        {
+            return HandType.FourOfAKind;
+        }
+        
+        if (countsSet.Contains(3) && countsSet.Contains(2))
+        {
+            return HandType.FullHouse;
+        }
+        
+        if (countsSet.Contains(3))
+        {
+            return HandType.ThreeOfAKind;
+        }
+        
+        if (countsSet.Contains(2) && cardCounts.Values.Count(v => v == 2) == 2)
+        {
+            return HandType.TwoPair;
+        }
+        
+        if (countsSet.Contains(2))
+        {
+            return HandType.Pair;
+        }
+        
+        return HandType.HighCard;
+    }
+
+    public Hand(string value, long bid, bool jacksWild)
+    {
+        Value = value;
+        Bid = bid;
+        JacksWild = jacksWild;
+        Type = GetType(value);
+    }
+    
+    private long GetCardValue(char card)
+    {
+        switch (card)
+        {
+            case 'A':
+                return 14;
+            case 'K':
+                return 13;
+            case 'Q':
+                return 12;
+            case 'J':
+                if (JacksWild)
+                {
+                    return 1;
+                }
+                return 11;
+            case 'T':
+                return 10;
+            default:
+                return long.Parse(card.ToString());
+        }
+    }
+    
+    public int CompareTo(Hand? other)
+    {
+        if (other == null)
+        {
+            return 1;
+        }
+        
+        var typeCompare = Type.CompareTo(other.Type);
+        if (typeCompare != 0)
+        {
+            return typeCompare;
+        }
+
+        for (int i = 0; i < Value.Length; i++)
+        {
+            var cardCompare = GetCardValue(Value[i]).CompareTo(GetCardValue(other.Value[i]));
+            if (cardCompare != 0)
+            {
+                return cardCompare;
+            }
+        }
+
+        return 0;
+    }
+}
