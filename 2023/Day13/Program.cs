7c111596d6c5f56115ac08c425ace66a295089ec
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-14 17:48:20 -0700

    Day 13 2023

--- 2023/Day13/Program.cs
+++ 2023/Day13/Program.cs
@@ -1,13 +1,95 @@
 ﻿using Utilities;
 
+IEnumerable<Grid> ParseGrids(List<string> lines)
+{
+    var grids = string.Join("\n", lines)
+        .Split("\n\n")
+        .Select(l => l.Split("\n"))
+        .Select(ParseGrid);
+
+    return grids;
+}
+
+Grid ParseGrid(string[] gridLines)
+{
+    // Turn each row / column into a bitmask
+    // starting from the 1st row/col look for reflections
+    
+    var rows = new List<long>();
+    var cols = new List<long>();
+    
+    // There is probably a fancier way to do this that doesn't require two passes
+    for (int i = 0; i < gridLines.Length; i++)
+    {
+        long row = 0;
+        for (int j = 0; j < gridLines[0].Length; j++)
+        {
+            row = (row << 1) + (gridLines[i][j] == '#' ? 1 : 0);
+        }
+        rows.Add(row);
+    }
+    
+    for (int j = 0; j < gridLines[0].Length; j++)
+    {
+        long col = 0;
+        for (int i = 0; i < gridLines.Length; i++)
+        {
+            col = (col << 1) + (gridLines[i][j] == '#' ? 1 : 0);
+        }
+        cols.Add(col);
+    }
+
+    return new Grid(rows, cols);
+}
+
+int CountDifferentBits(long a, long b)
+{
+    var diff = a ^ b;
+    return System.Numerics.BitOperations.PopCount((ulong)diff);
+}
+
+// Returns index of reflection in the input and 0 if no reflection is found
+long FindReflection(List<long> input, long allowedDifferences)
+{
+    var differences = new List<(long differences, long index)>();
+    for (var i = 1; i < input.Count; i++)
+    {
+        var numDifferences = 0;
+        var numToCheck = Math.Min(i, input.Count - i);
+        for (var j = 0; j < numToCheck; j++)
+        {
+            numDifferences += CountDifferentBits(input[i + j], input[i - j - 1]);
+        }
+        
+        differences.Add((numDifferences, i));
+    }
+
+    // Assumes there is only ever one line of reflection
+    return differences.Where((tuple => tuple.differences == allowedDifferences)).FirstOrDefault((0,0)).index;
+}
+
+long GetReflectionScore(Grid grid, long allowedDifferences)
+{
+    long verticalReflection = FindReflection(grid.Columns, allowedDifferences);
+    if (verticalReflection > 0)
+    {
+        return verticalReflection;
+    }
+
+    long horizontalReflection = FindReflection(grid.Rows, allowedDifferences);
+    return 100 * horizontalReflection;
+}
+
 long Part1(List<string> lines)
 {
-    return 0;
+    var grids = ParseGrids(lines);
+    return grids.Select(g => GetReflectionScore(g, 0)).Sum();
 }
 
 long Part2(List<string> lines)
 {
-    return 0;
+    var grids = ParseGrids(lines);
+    return grids.Select(g => GetReflectionScore(g, 1)).Sum();
 }
 
 var lines = new List<string>(File.ReadAllLines("input.txt"));
@@ -21,3 +103,9 @@ Runner.Benchmark(delegate
     Part1(lines);
     Part2(lines);
 }, "Day 13");
+
+public class Grid(List<long> rows, List<long> cols)
+{
+    public List<long> Rows { get; } = rows;
+    public List<long> Columns { get; } = cols;
+}
