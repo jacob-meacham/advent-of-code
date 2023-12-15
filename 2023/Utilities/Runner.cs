2b415020590ba815e086f2c62372c85dbc6fd3e5
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-15 13:27:19 -0700

    Make runner a bit smarter

--- 2023/Utilities/Runner.cs
+++ 2023/Utilities/Runner.cs
@@ -1,9 +1,14 @@
 ﻿using System.Diagnostics;
+using System.Globalization;
 
 namespace Utilities;
 
 public static class Runner
 {
+    private static string NumSpaces(string label, int total)
+    {
+        return new string(' ', total-label.Length);
+    }
     public static void Benchmark(Action fn, String day, int iterations = 10)
     {
         Stopwatch stopwatch = Stopwatch.StartNew();
@@ -15,6 +20,6 @@ public static class Runner
         
         var avgMilliseconds = (float)stopwatch.ElapsedMilliseconds / iterations;
         var good = avgMilliseconds < 250 ? "✅" : "❌";
-        Console.WriteLine($"| {day}  | {avgMilliseconds}        |{good}      |");
+        Console.WriteLine($"| {day}{NumSpaces(day, 7)}| {avgMilliseconds}{NumSpaces(avgMilliseconds.ToString(CultureInfo.InvariantCulture), 12)}|{good}     |");
     }
 }
