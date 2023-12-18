d996ff33517cb18442c9ebca65402ef2a092fbfa
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-18 09:24:19 -0700

    2023 Scaffolding

---
+++ 2023/Day18/Program.cs
@@ -0,0 +1,23 @@
+﻿using Utilities;
+
+long Part1(IReadOnlyList<string> lines)
+{
+    return 0;
+}
+
+long Part2(IReadOnlyList<string> lines)
+{
+    return 0;
+}
+
+var lines = new List<string>(File.ReadAllLines("input.txt"));
+var part1 = Part1(lines);
+var part2 = Part2(lines);
+
+Console.WriteLine($"Part 1: {part1}, Part 2: {part2}");
+
+Runner.Benchmark(delegate
+{
+    Part1(lines);
+    Part2(lines);
+}, "Day 18");
---
+++ 2023/Day25/Program.cs
@@ -0,0 +1,16 @@
+﻿using Utilities;
+
+long Part1(IReadOnlyList<string> lines)
+{
+    return 0;
+}
+
+var lines = new List<string>(File.ReadAllLines("input.txt"));
+var part1 = Part1(lines);
+
+Console.WriteLine($"Part 1: {part1}");
+
+Runner.Benchmark(delegate
+{
+    Part1(lines);
+}, "Day 25");
