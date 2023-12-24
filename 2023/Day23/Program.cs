6c73bd9359b131aec9ed81cd5560e787a2c11bc5
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-23 22:03:21 -0700

    Phew Day 23 2023

--- 2023/Day23/Program.cs
+++ 2023/Day23/Program.cs
@@ -28,74 +28,15 @@ void DebugPrint(Grid grid, List<Vec2> longestPath)
     }
 }
 
-bool CanMove(char c, Vec2 from, Vec2 to)
-{
-    var direction = from - to;
-    return c switch
-    {
-        '^' => direction.X != -1,
-        'v' => direction.X != 1,
-        '<' => direction.Y != -1,
-        '>' => direction.Y != 1,
-        '.' => true,
-        _ => false
-    };
-}
-
-bool IsSlope(char slope)
-{
-    return slope != '.' && slope != '#';
-}
-
 // Another graph problem!
 long Part1(IReadOnlyList<string> lines)
 {
-    var grid = Grid.CreateFromInput(lines, (c, _, _) => c);
+    var visitor = new Part1GraphVisitor();
+    
     var start = new Vec2(0, lines[0].IndexOf('.'));
-    var goal = new Vec2(grid.MaxRows, lines[^1].IndexOf('.'));
+    var goal = new Vec2(lines.Count-1, lines[^1].IndexOf('.'));
+    var graph = visitor.Create(lines, false);
     
-    var graph = new Graph<Vec2>();
-    var startNode = graph.AddNode(start);
-    
-    // Create our graph
-    var visited = new Dictionary<Vec2, Node<Vec2>>();
-    var searchStack = new Stack<Node<Vec2>>();
-    searchStack.Push(startNode);
-    
-    while (searchStack.TryPop(out var node))
-    {
-        visited.Add(node.Value, node);
-        var validDirections = grid.Get(node.Value) switch
-        {
-            '^' => [directions[3]],
-            'v' => [directions[2]],
-            '<' => [directions[1]],
-            '>' => [directions[0]],
-            _ => directions
-        };
-
-        var validNeighbors = validDirections
-            .Select(d => node.Value + d)
-            .Where(p => grid.Contains(p) && CanMove(grid.Get(p), node.Value, p));
-        
-        foreach (var neighbor in validNeighbors)
-        {
-            if (visited.TryGetValue(neighbor, out var value))
-            {
-                if (IsSlope(grid.Get(node.Value)))
-                {
-                    // Already added the node from some other path, just need to add it to our children
-                    node.AddEdge(value);
-                }
-            }
-            else
-            {
-                var neighborNode = graph.AddNode(neighbor, node);
-                searchStack.Push(neighborNode);
-            }
-        }
-    }
-
     var longestPath = graph.GetLongestPath(start, goal);
     //DebugPrint(grid, longestPath);
     
@@ -106,7 +47,15 @@ long Part2(IReadOnlyList<string> lines)
 {
     // We no longer have a DAG, so we can't just topologically sort.
     // There are only 4 nodes that connect to more than 1 path so we can make weighted edges
-    return 0;
+    var visitor = new Part2GraphVisitor();
+    
+    var goal = new Vec2(lines.Count-1, lines[^1].IndexOf('.'));
+    var graph = visitor.Create(lines, true);
+    
+    graph.CollapseNodes();
+    var maxDist = graph.Dfs(graph.Nodes[0], goal);
+
+    return maxDist;
 }
 
 var lines = new List<string>(File.ReadAllLines("input.txt"));
@@ -119,36 +68,241 @@ Runner.Benchmark(delegate
 {
     Part1(lines);
     Part2(lines);
-}, "Day 23");
+}, "Day 23", 2);
 
-internal class Node<T>(T value)
+internal abstract class GraphVisitor
 {
-    public T Value { get; } = value;
-    public Dictionary<Node<T>, int> Incoming { get; } = new();
-    public Dictionary<Node<T>, int> Outgoing { get; } = new();
+    protected abstract List<Vec2> ValidDirections(char c);
+    protected abstract bool IsValidNeighbor(Grid grid, Vec2 curPos, Vec2 nextPos);
 
-    public List<Node<T>> Children => Outgoing.Keys.ToList();
+    protected abstract void VisitNeighbor(Grid grid, Dictionary<Vec2, Node<Vec2>> nodes, Node<Vec2> node, Vec2 neighbor,
+        Stack<Node<Vec2>> searchStack, bool biDirectional);
 
-    public void AddEdge(Node<T> node, int weight = 1)
+    internal Graph<Vec2> Create(IReadOnlyList<string> lines, bool biDirectional)
     {
-        Outgoing[node] = weight;
-        node.Incoming[this] = weight;
+        var grid = Grid.CreateFromInput(lines, (c, _, _) => c);
+        var start = new Vec2(0, lines[0].IndexOf('.'));
+        var graph = new Graph<Vec2>(biDirectional);
+    
+        // Create our graph
+        var nodes = new Dictionary<Vec2, Node<Vec2>>();
+        var searchStack = new Stack<Node<Vec2>>();
+        searchStack.Push(new Node<Vec2>(start));
+    
+        while (searchStack.TryPop(out var node))
+        {
+            nodes.TryAdd(node.Value, node);
+            
+            var c = grid.Get(node.Value);
+            var validDirections = ValidDirections(c);
+
+            var validNeighbors = validDirections
+                .Select(d => node.Value + d)
+                .Where(p => IsValidNeighbor(grid, node.Value, p));
+        
+            foreach (var neighbor in validNeighbors)
+            {
+                VisitNeighbor(grid, nodes, node, neighbor, searchStack, biDirectional);
+            }
+        }
+
+        foreach (var node in nodes)
+        {
+            graph.AddNode(node.Value);
+        }
+
+        return graph;
     }
 }
 
-internal class Graph<T>
+internal class Part1GraphVisitor : GraphVisitor
+{
+    protected override List<Vec2> ValidDirections(char c)
+    {
+        return c switch
+        {
+            '^' => [Statics.Directions[3]],
+            'v' => [Statics.Directions[2]],
+            '<' => [Statics.Directions[1]],
+            '>' => [Statics.Directions[0]],
+            _ => Statics.Directions
+        };
+    }
+
+    protected override bool IsValidNeighbor(Grid grid, Vec2 curPos, Vec2 nextPos)
+    {
+        if (!grid.Contains(nextPos))
+        {
+            return false;
+        }
+
+        var c = grid.Get(nextPos);
+        var direction = curPos - nextPos;
+        return c switch
+        {
+            '^' => direction.X != -1,
+            'v' => direction.X != 1,
+            '<' => direction.Y != -1,
+            '>' => direction.Y != 1,
+            '.' => true,
+            _ => false
+        };
+    }
+
+    protected override void VisitNeighbor(Grid grid, Dictionary<Vec2, Node<Vec2>> nodes, Node<Vec2> node, 
+        Vec2 neighbor, Stack<Node<Vec2>> searchStack, bool biDirectional)
+    {
+        if (nodes.TryGetValue(neighbor, out var value))
+        {
+            var c = grid.Get(node.Value);
+            if (c != '.' && c != '#')
+            {
+                // Already added the node from some other path non-directional path,
+                // just need to add it to our children
+                node.AddEdge(value, false);
+            }
+        }
+        else
+        {
+            var neighborNode = new Node<Vec2>(neighbor);
+            node.AddEdge(neighborNode, false);
+            searchStack.Push(neighborNode);
+        }
+    }
+}
+
+internal class Part2GraphVisitor : GraphVisitor
+{
+    protected override List<Vec2> ValidDirections(char c)
+    {
+        return Statics.Directions;
+    }
+
+    protected override bool IsValidNeighbor(Grid grid, Vec2 curPos, Vec2 nextPos)
+    {
+        return grid.Contains(nextPos) && grid.Get(nextPos) != '#';
+    }
+
+    protected override void VisitNeighbor(Grid grid, Dictionary<Vec2, Node<Vec2>> nodes, Node<Vec2> node, Vec2 neighbor, Stack<Node<Vec2>> searchStack, bool biDirectional)
+    {
+        if (!nodes.TryGetValue(neighbor, out var neighborNode))
+        {
+            neighborNode = new Node<Vec2>(neighbor);
+            nodes.Add(neighbor, neighborNode);
+            
+            searchStack.Push(neighborNode);
+        }
+        
+        node.AddEdge(neighborNode, biDirectional);
+    }
+}
+
+internal class Node<T>(T value)
+{
+    public T Value { get; set;  } = value;
+
+    public List<(Node<T> node, int weight)> Neighbors { get; } = [];
+
+    public void AddEdge(Node<T> neighbor, bool biDirectional)
+    {
+        if (Neighbors.All(tuple => tuple.node != neighbor))
+        {
+            Neighbors.Add((neighbor, 1));
+        }
+
+        if (!biDirectional)
+        {
+            return;
+        }
+        
+        if (neighbor.Neighbors.All(tuple => tuple.node != this))
+        {
+            neighbor.Neighbors.Add((this, 1));
+        }
+    }
+}
+
+internal class Graph<T>(bool biDirectional)
     where T : class
 {
     public List<Node<T>> Nodes { get; } = [];
 
-    public Node<T> AddNode(T value, Node<T>? parentNode = null, int weight = 1)
+    public Node<T> AddNode(T value, Node<T>? fromNode = null)
     {
         var node = new Node<T>(value);
-        parentNode?.AddEdge(node, weight);
+        fromNode?.AddEdge(node, biDirectional);
 
         Nodes.Add(node);
-
         return node;
+    }
+    
+    public void AddNode(Node<T> node)
+    {
+        Nodes.Add(node);
+    }
+    
+    // Assumes the graph is fully connected
+    public void CollapseNodes()
+    {
+        void ReplaceNeighbor(Node<T> nodeToReplace, Node<T> nodeToSearch, (Node<T> node, int weight) newNode)
+        {
+            for (var i = 0; i < nodeToSearch.Neighbors.Count; i++)
+            {
+                if (nodeToSearch.Neighbors[i].node != nodeToReplace)
+                {
+                    continue;
+                }
+                nodeToSearch.Neighbors[i] = (newNode.node, nodeToSearch.Neighbors[i].weight + newNode.weight);
+                break;
+            }
+        }
+        var removedNodes = new List<Node<T>>();
+        var corridors = Nodes.Where(n => n.Neighbors.Count == 2);
+        foreach (var n in corridors)
+        {
+            ReplaceNeighbor(n, n.Neighbors[0].node, n.Neighbors[1]);
+            ReplaceNeighbor(n, n.Neighbors[1].node, n.Neighbors[0]);
+            
+            removedNodes.Add(n);
+        }
+       
+        foreach (var node in removedNodes)
+        {
+            Nodes.Remove(node);
+        }
+    }
+
+    public long Dfs(Node<T> start, T goal)
+    {
+        var visited = new HashSet<T>();
+        return DfsHelper(visited, start, goal);
+    }
+
+    private static long DfsHelper(ISet<T> visited, Node<T> start, T goal)
+    {
+        if (start.Value.Equals(goal))
+        {
+            return 0L;
+        }
+        
+        var maxDist = -1L;
+        foreach (var (node, weight) in start.Neighbors)
+        {
+            if (!visited.Add(node.Value))
+            {
+                continue;
+            }
+            
+            var dist = DfsHelper(visited, node, goal);
+            if (dist != -1)
+            {
+                // Found a way to the goal
+                maxDist = Math.Max(maxDist, dist + weight);    
+            }
+            visited.Remove(node.Value);
+        }
+
+        return maxDist;
     }
 
     private List<Node<T>> TopologicalSort()
@@ -174,7 +328,7 @@ internal class Graph<T>
             return;
         }
         
-        foreach (var child in node.Children)
+        foreach (var (child, _) in node.Neighbors)
         {
             TopologicalSortUtil(child, visited, stack);
         }
@@ -196,8 +350,8 @@ internal class Graph<T>
         longestPath[start] = 0;
         foreach (var node in topologicalSort)
         {
-            foreach (var child in node.Children
-                         .Where(child => longestPath[child.Value] <= longestPath[node.Value] + 1))
+            foreach (var (child, _) in node.Neighbors
+                         .Where(child => longestPath[child.node.Value] <= longestPath[node.Value] + 1))
             {
                 longestPath[child.Value] = longestPath[node.Value] + 1;
             }
@@ -210,4 +364,15 @@ internal class Graph<T>
 
         return longestPath.Values.Max();
     }
+}
+
+internal static class Statics
+{
+    internal static readonly List<Vec2> Directions =
+    [
+        new Vec2(0, 1),
+        new Vec2(0, -1),
+        new Vec2(1, 0),
+        new Vec2(-1, 0)
+    ];
 }
