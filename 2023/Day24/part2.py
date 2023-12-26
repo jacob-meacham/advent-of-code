fa810f43bf17bbbd8c540afce02b4a0b66fe8ec5
Jacob Meacham <jacob.e.meacham@gmail.com>
2023-12-25 17:33:01 -0700

    Day 25 2023

--- 2023/Day24/part2.py
+++ 2023/Day24/part2.py
@@ -1,33 +1,50 @@
 import z3
+import time
 
-rays = []
-with (open('input.txt', 'r') as f):
-    for l in f.readlines():
-        l = l.strip()
-        pos_part, vel_part = l.split(' @ ')
-        pos = [int(p) for p in pos_part.split(', ')]
-        vel = [int(v) for v in vel_part.split(', ')]
-        rays.append(((pos[0], pos[1], pos[2]), (vel[0], vel[1], vel[2])))
+def benchmark(fn, iterations):
+    start = time.perf_counter()
+    for _ in range(0, iterations):
+        answer = fn()
+    end = time.perf_counter()
+    return 1000 * ((end - start) / iterations), answer
 
 
-x, y, z = z3.BitVec('x', 64), z3.BitVec('y', 64), z3.BitVec('z', 64)
-vx, vy, vz = z3.BitVec('vx', 64), z3.BitVec('vy', 64), z3.BitVec('vz', 64)
 
-solver = z3.Solver()
 
-for i, ray in enumerate(rays):
-    (px, py, pz), (pvx, pvy, pvz) = ray
+def main():
+    rays = []
+    with (open('input.txt', 'r') as f):
+        for l in f.readlines():
+            l = l.strip()
+            pos_part, vel_part = l.split(' @ ')
+            pos = [int(p) for p in pos_part.split(', ')]
+            vel = [int(v) for v in vel_part.split(', ')]
+            rays.append(((pos[0], pos[1], pos[2]), (vel[0], vel[1], vel[2])))
 
-    t = z3.BitVec(f't_{i}', 64)
-    solver.add(t >= 0)
-    solver.add(x + vx * t == px + pvx * t)
-    solver.add(y + vy * t == py + pvy * t)
-    solver.add(z + vz * t == pz + pvz * t)
 
-assert solver.check() == z3.sat
+    x, y, z = z3.BitVec('x', 64), z3.BitVec('y', 64), z3.BitVec('z', 64)
+    vx, vy, vz = z3.BitVec('vx', 64), z3.BitVec('vy', 64), z3.BitVec('vz', 64)
 
-m = solver.model()
-x, y, z = m.eval(x), m.eval(y), m.eval(z)
-x, y, z = x.as_long(), y.as_long(), z.as_long()
+    solver = z3.Solver()
 
-print(f'Part 2: {x + y + z}')
+    # Same answer for a much smaller number of positions, which could probably be done without a solver
+    for i, ray in enumerate(rays[0:3]):
+        (px, py, pz), (pvx, pvy, pvz) = ray
+
+        t = z3.BitVec(f't_{i}', 64)
+        solver.add(t >= 0)
+        solver.add(x + vx * t == px + pvx * t)
+        solver.add(y + vy * t == py + pvy * t)
+        solver.add(z + vz * t == pz + pvz * t)
+
+    assert solver.check() == z3.sat
+
+    m = solver.model()
+    x, y, z = m.eval(x), m.eval(y), m.eval(z)
+    x, y, z = x.as_long(), y.as_long(), z.as_long()
+
+    return x + y + z
+
+avg_millis, answer = benchmark(main, 10)
+print(f'Part 2 Answer: {answer}')
+print(f'  {"✅" if avg_millis < 500 else "❌"} average ms: {avg_millis:.2f}')
--- 2023/Day25/Program.cs
+++ 2023/Day25/Program.cs
@@ -1,8 +1,70 @@
 ﻿using Utilities;
 
+var random = new Random();
+
+(int remainingEdges, int partA, int partB) Kargers(Graph graph, List<int> vertices)
+{
+    var componentCount = vertices.Select(idx => new KeyValuePair<int, int>(idx, 1)).ToDictionary();
+    
+    // Use https://en.wikipedia.org/wiki/Karger%27s_algorithm to determine a probabilistic min cut
+    while (vertices.Count > 2)
+    {
+        // Pick a random edge
+        var v1 = vertices[random.Next(vertices.Count)];
+        var edges = graph.GetEdges(v1);
+        var v2 = edges[random.Next(edges.Count)];
+        
+        graph.ContractEdge(v1, v2);
+        
+        componentCount[v1] += componentCount[v2];
+        componentCount.Remove(v2);
+        vertices.Remove(v2);
+    }
+
+    return (graph.GetEdges(vertices[0]).Count, componentCount[vertices[0]], componentCount[vertices[1]]);
+}
+
 long Part1(IReadOnlyList<string> lines)
 {
-    return 0;
+    Dictionary<string, int> idToIdx = new();
+    
+    var curVertIdx = 0;
+
+    void AddVert(string name)
+    {
+        if (idToIdx.TryAdd(name, curVertIdx))
+        {
+            curVertIdx++;
+        }
+    }
+    
+    foreach (var l in lines)
+    {
+        var (vertex, adjs) = l.Split(": ");
+        var all = new[] { vertex }.Concat(adjs.Split(" "));
+        foreach (var vert in all)
+        {
+            AddVert(vert);
+        }
+    }
+    
+    // Could probably do this in one pass but knowing the size is useful for the adjacency list
+    var graph = new Graph(idToIdx.Count);
+    foreach (var l in lines)
+    {
+        var (vertex, adjs) = l.Split(": ");
+        graph.AddEdges(idToIdx[vertex], adjs.Split(" ").Select(a => idToIdx[a]));    
+    }
+    
+    var vertices = idToIdx.Values.ToList();
+    while (true)
+    {
+        var (remainingEdges, partA, partB) = Kargers(graph.Clone(), new List<int>(vertices));
+        if (remainingEdges == 3)
+        {
+            return partA * partB;
+        }
+    }
 }
 
 var lines = new List<string>(File.ReadAllLines("input.txt"));
@@ -14,3 +76,70 @@ Runner.Benchmark(delegate
 {
     Part1(lines);
 }, "Day 25");
+
+internal class Graph
+{
+    private readonly List<int>[] _adjacencyList;
+
+    public Graph(int vertices)
+    {
+        _adjacencyList = new List<int>[vertices];
+        for (var i = 0; i < vertices; i++)
+        {
+            _adjacencyList[i] = [];
+        }
+    }
+
+    public void AddEdges(int vert, IEnumerable<int> neighbors)
+    {
+        foreach (var other in neighbors)
+        {
+            _adjacencyList[vert].Add(other);
+            _adjacencyList[other].Add(vert);
+        }
+    }
+
+    public List<int> GetEdges(int vert)
+    {
+        return _adjacencyList[vert];
+    }
+    
+    public bool IsConnected(int vertex1, int vertex2)
+    {
+        return _adjacencyList[vertex1].Contains(vertex2) && _adjacencyList[vertex2].Contains(vertex1);
+    }
+
+    public void ContractEdge(int v1, int v2)
+    {
+        if (!IsConnected(v1, v2))
+        {
+            throw new ArgumentException($"Vertices {v1} and {v2} are not connected. Cannot contract edge.");
+        }
+        
+        foreach (var vertex in _adjacencyList[v2])
+        {
+            _adjacencyList[vertex].Remove(v2);
+         
+            // Avoid self-loop
+            if (vertex == v1)
+            {
+                continue;
+            }
+            _adjacencyList[v1].Add(vertex);
+            _adjacencyList[vertex].Add(v1);
+        }
+        
+        _adjacencyList[v2].Clear();
+    }
+
+    public Graph Clone()
+    {
+        var clone = new Graph(_adjacencyList.Length);
+        for (var i = 0; i < _adjacencyList.Length; i++)
+        {
+            clone._adjacencyList[i] = [.._adjacencyList[i]];
+        }
+
+        return clone;
+    }
+}
