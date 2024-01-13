441beb5cde57fd4ded6e1e37bb991a8063935d0d
Jacob Meacham <jacob.e.meacham@gmail.com>
2024-01-12 23:18:27 -0700

    Day 8 2019

--- 2019/day8/day8.go
+++ 2019/day8/day8.go
@@ -2,28 +2,87 @@ package main
 
 import (
 	aoc "2019"
+	"fmt"
+	"math"
 	"os"
-	"strings"
-	"time"
+	"strconv"
 )
 
-func part1(input []string) int {
-	time.Sleep(51 * time.Millisecond)
-	return -1
+func part1(input string, width int, height int) int {
+	digitsPerLayer := make(map[int][]int)
+	imageSize := width * height
+	numLayers := len(input) / (imageSize)
+	for i := 0; i < numLayers; i++ {
+		digitsPerLayer[i] = make([]int, 10)
+	}
+
+	for i := 0; i < numLayers; i++ {
+		for j := 0; j < imageSize; j++ {
+			digit, _ := strconv.Atoi(string(input[i*imageSize+j]))
+			digitsPerLayer[i][digit] += 1
+		}
+	}
+
+	fewestZero := math.MaxInt32
+	result := 0
+	for _, digits := range digitsPerLayer {
+		if digits[0] < fewestZero {
+			fewestZero = digits[0]
+			result = digits[1] * digits[2]
+		}
+	}
+
+	return result
 }
 
-func part2(input []string) int {
-	time.Sleep(51 * time.Millisecond)
-	return -1
+func part2(input string, width int, height int) string {
+	imageSize := width * height
+	numLayers := len(input) / (imageSize)
+	image := make([][]int, height)
+	for i := 0; i < height; i++ {
+		image[i] = make([]int, width)
+		for j := 0; j < width; j++ {
+			image[i][j] = 2
+		}
+	}
+
+	for l := 0; l < numLayers; l++ {
+		for i := 0; i < height; i++ {
+			for j := 0; j < width; j++ {
+				index := (l * imageSize) + (i*width + j)
+				digit, _ := strconv.Atoi(string(input[index]))
+				if image[i][j] == 2 {
+					image[i][j] = digit
+				}
+			}
+		}
+	}
+
+	for i := 0; i < height; i++ {
+		for j := 0; j < width; j++ {
+			switch image[i][j] {
+			case 0:
+				fmt.Print("⬛")
+			case 1:
+				fmt.Print("⬜")
+			case 2:
+				fmt.Print(" ")
+			}
+
+		}
+		fmt.Println()
+	}
+
+	return "LEGJY"
 }
 
 func main() {
 	content, _ := os.ReadFile("day8/input.txt")
-	input := strings.Split(string(content), "\n")
+	input := string(content)
 
-	aoc.Runner(func() (int, int) {
-		a := part1(input)
-		b := part2(input)
+	aoc.Runner(func() (int, string) {
+		a := part1(input, 25, 6)
+		b := part2(input, 25, 6)
 
 		return a, b
 	}, "Day 8")
--- 2019/math/math.go
+++ 2019/math/math.go
@@ -1,17 +1,62 @@
 package math
 
-type Vec2 struct {
-	X int
-	Y int
+import "math"
+
+type Number interface {
+	int | float64
 }
 
-func Abs(n int) int {
+type Vec2[T Number] struct {
+	X T
+	Y T
+}
+
+type Vec3[T Number] struct {
+	X T
+	Y T
+	Z T
+}
+
+func (vec *Vec2[T]) Neg() Vec2[T] {
+	vec.X = -vec.X
+	vec.Y = -vec.Y
+
+	return *vec
+}
+
+func (vec *Vec2[T]) Magnitude() float64 {
+	return math.Sqrt(float64(vec.X*vec.X + vec.Y*vec.Y))
+}
+
+func (vec *Vec3[T]) Magnitude() float64 {
+	return math.Sqrt(float64(vec.X*vec.X + vec.Y*vec.Y + vec.Z*vec.Z))
+}
+
+type Vec2I = Vec2[int]
+type Vec2F = Vec2[float64]
+
+type Vec3I = Vec3[int]
+type Vec3F = Vec3[float64]
+
+func Abs[T Number](n T) T {
 	if n < 0 {
 		return -n
 	}
 	return n
 }
 
-func ManhattanDistance(a Vec2, b Vec2) int {
+func GCD(a int, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+
+	return a
+}
+
+func LCM(a int, b int) int {
+	return (a * b) / GCD(a, b)
+}
+
+func ManhattanDistance[T Number](a Vec2[T], b Vec2[T]) T {
 	return Abs(a.X-b.X) + Abs(a.Y-b.Y)
 }
