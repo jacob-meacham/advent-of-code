1b4b6718c508dbff325cad3068e21b0f8df3285e
Jacob Meacham <jacob.e.meacham@gmail.com>
2024-12-01 16:39:59 -0700

    2024 Day 1 :tada:

--- 2024/README.md
+++ 2024/README.md
@@ -1,1 +1,1 @@
-Try out elixir!
+Figure out the best way to organize so that it's easy to run a single one, loading input is not part of the benchmarking and it's easy to run all
---
+++ 2024/lib/day1/day1.ex
@@ -0,0 +1,46 @@
+defmodule Day1 do
+  @moduledoc "Day 1"
+
+  def parse_input(input) do
+    numbers = input |> String.split("\n", trim: true) |>
+      Enum.map(fn i -> i |> String.split("   ", trim: true) end) |>
+      Enum.map(fn [a, b] -> [String.to_integer(a), String.to_integer(b)] end) |>
+      Enum.map(&List.to_tuple/1)
+
+    {l1, l2} = Enum.unzip(numbers)
+    l1 = Enum.sort(l1)
+    l2 = Enum.sort(l2)
+
+    {l1, l2}
+  end
+
+  def part1(input) do
+    {l1, l2} = parse_input(input)
+    result = Enum.zip(l1, l2) |>
+    Enum.reduce(0, fn {a, b}, acc -> abs(a - b) + acc end)
+
+    result
+  end
+
+  def part2(input) do
+    {l1, l2} = parse_input(input)
+    {result, _} = Enum.reduce(l1, {0, 0}, fn item, {cum, start} ->
+      possible_set = Enum.slice(l2, start..length(l2)) |> Enum.take_while(fn i -> i <= item end)
+      num_equal = Enum.filter(possible_set, fn i -> i == item end) |> length()
+
+      {cum + item * num_equal, start + length(possible_set) - num_equal}
+    end)
+
+    result
+  end
+end
+
+# TODO: Figure out better way of reading...
+{:ok, contents} = File.read("lib/day1/input.txt")
+IO.puts(Day1.part1(contents))
+IO.puts(Day1.part2(contents))
+
+# TODO: Want something better than this if I stay with Elixir
+Benchee.run(%{part_1: fn -> contents |> Day1.part1() end,
+             part_2: fn -> contents |> Day1.part2() end}, warmup: 2,
+time: 3)
