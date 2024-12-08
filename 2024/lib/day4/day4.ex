0e73fc8482492eb76dae455d5af6122cb7c8abe9
Jacob Meacham <jacob.e.meacham@gmail.com>
2024-12-07 19:26:49 -0700

    2024 Day 4 :lips:

--- 2024/lib/day4/day4.ex
+++ 2024/lib/day4/day4.ex
@@ -1,17 +1,74 @@
 defmodule WordSearch do
   @moduledoc "Day 4"
 
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.with_index
+    |> Enum.flat_map(fn {line, row} ->
+      String.split(line, "")
+      |> Enum.with_index
+      |> Enum.flat_map(fn {char, col} ->
+        position = {row, col}
+        [{position, char}]
+      end)
+    end)
+    |> Map.new
+  end
+
+  defp directions do
+    [
+      {1, 0},
+      {-1, 0},
+      {0, 1},
+      {0, -1},
+      {1, 1},
+      {-1, -1},
+      {1, -1},
+      {-1, 1}
+    ]
+  end
 
   def part1(input) do
-    # Parse into map that represents 2d array
-    # for each X in the array
-    # do a ray cast in the 8 directions to determine if any full matches
-    # can be recursive
-    0
+    grid = parse(input)
+    grid
+    |> Enum.filter(fn {_, c} -> c == "X" end)
+    |> Enum.reduce(0, fn {position, _}, count ->
+      # Check in each direction
+      directions()
+        |> Enum.reduce(count, fn direction, count ->
+          case matches_word?(["X", "M", "A", "S"], grid, position, direction) do
+            true -> count + 1
+            false -> count
+          end
+        end)
+    end)
+  end
+
+  defp matches_word?([], _, _, _) do
+    true
+  end
+  defp matches_word?([c|word], grid, {px, py}, {dx, dy}) do
+    case Map.get(grid, {px, py}, "#") do
+      ^c ->
+        matches_word?(word, grid, {px + dx, py + dy}, {dx, dy})
+      _ ->
+        false
+    end
   end
 
   def part2(input) do
-    0
+    grid = parse(input)
+    grid
+    |> Enum.filter(fn {_, c} -> c == "A" end)
+    |> Enum.reduce(0, fn {{px, py}, _}, count ->
+      [ul, ur, ll, lr] = [{-1, -1}, {1, -1}, {-1, 1}, {1, 1}] |> Enum.map(fn {dx, dy} -> Map.get(grid, {px + dx, py + dy}, "#") end)
+      case ([ul, lr] == ["S", "M"] or [ul, lr] == ["M", "S"]) and
+      ([ur, ll] == ["S", "M"] or [ur, ll] == ["M", "S"]) do
+        true -> count + 1
+        false -> count
+      end
+    end)
   end
 end
 
@@ -20,7 +77,11 @@ IO.puts(WordSearch.part1(contents))
 IO.puts(WordSearch.part2(contents))
 
 
-# Benchee.run(%{day4_part1: fn -> contents |> WordSearch.part1() end,
-#             day4_part2: fn -> contents |> WordSearch.part2() end,
-#              }, warmup: 2,
-# time: 5)
+Benchee.run(%{
+  day4_part1: fn -> contents |> WordSearch.part1() end,
+  day4_part2: fn -> contents |> WordSearch.part2() end,
+  day4_total: fn ->
+    WordSearch.part1(contents)
+    WordSearch.part2(contents)
+  end}, warmup: 2,
+time: 3)
